c8be74fc850a6e4191fc90e4784bd3bfb858b6ff
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-08-05 18:43:51 -0500

    created all files and folders for navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './App.css'
 
 function App() {
 
   return(
     <div>
-        <h1>Hello React</h1>
+        <h1 className='name'>SquareHelp</h1>
     </div>
   );
 }
