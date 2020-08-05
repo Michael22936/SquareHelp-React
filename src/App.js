8bf793bef7e8d3f6a21e10b8d43bf4ba429577b1
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-08-05 18:29:51 -0500

    removed all basic react components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react';
 
 function App() {
 
-  const sayHello = () =>{
-    console.log('Hello');
-  };
-
   return(
     <div>
         <h1>Hello React</h1>
-      <button onClick={sayHello}>Hello</button>
     </div>
   );
 }
