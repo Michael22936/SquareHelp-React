cd722b7c071427b48c2dc365b689a08825facbd0
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-08-05 19:04:59 -0500

    added react-bootstrap and added a react navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
 
   return(
-    <div>
-        <h1 className='name'>SquareHelp</h1>
-    </div>
+      < Navbar/>
   );
 }
 
