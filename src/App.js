4b7e198f5e772d1681b4164b9949d703a98c3930
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-08-13 22:38:01 -0500

    started working on landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Landing from "./components/landing/Landing";
 
 function App() {
 
   return(
       < Navbar/>
+  );
+  return (
+  <Landing/>
   );
 }
 
